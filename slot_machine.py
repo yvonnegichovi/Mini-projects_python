f7a88af153eb3dceddbdfc4600b3f4cadf498780
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-13 09:26:18 -0800

    slot machine with profiles

--- slot_machine.py
+++ slot_machine.py
@@ -22,111 +22,146 @@ symbol_value = {
     "C": 3,
     "D": 2
 }
-def check_winnings(columns, lines, bet, values):
-    winnings = 0
-    winning_lines = []
-    for line in range(lines):
-        symbol = columns[0][line]
-        for column in columns:
-            symbol_to_check = column[line]
-            if symbol != symbol_to_check:
-                break
-        else:
-            winnings = values[symbol] * bet
-            winning_lines.append(lines + 1)
 
-    return winnings, winning_lines
+class Profile:
+    def __init__(self, name, balance=100):
+        self.name = name
+        self.balance = balance
 
-def get_slot_machine_spin(rows, cols, symbols):
-    all_symbols = []
-    for symbol, symbol_count in symbols.items():
-        for _ in range(symbol_count):
-            all_symbols.append(symbol)
-    columns = []
-    for col in range(cols):
-        column = []
-        current_symbols = all_symbols[:]
-        for row in range(rows):
-            value = random.choice(current_symbols)
-            current_symbols.remove(value)
-            column.append(value)
-        columns.append(column)
-
-    return columns
-
-def print_slot_machine(columns):
-    for row in range(len(columns[0])):
-        for i, column in enumerate(columns):
-            if i != len(columns) - 1:
-                print(column[row], end=" | ")
+    def __str__(self):
+        return f"Profile: {self.name}, Balance: ${self.balance}"
+    def deposit(self):
+        while True:
+            amount = input("What would you like to deposit? $")
+            if amount.isdigit():
+                amount = int(amount)
+                if amount > 0:
+                    break
+                else:
+                    print("Amount must be greater than 0.")
             else:
-                print(column[row], end="")
-        print()
+                print("Please enter a number.")
+        self.balance += amount
 
-def deposit():
-    while True:
-        amount = input("What would you like to deposit? $")
-        if amount.isdigit():
-            amount = int(amount)
-            if amount > 0:
-                break
+class SlotMachine:
+    def __init__(self):
+        self.profiles = []
+    def create_profile(self, name, initial_balance=100):
+        profile = Profile(name, initial_balance)
+        self.profiles.append(profile)
+        return profile
+    def display_profiles(self):
+        for profile in self.profiles:
+            print(profile)
+
+    def check_winnings(self, columns, lines, bet, values):
+        winnings = 0
+        winning_lines = []
+        for line in range(lines):
+            symbol = columns[0][line]
+            for column in columns:
+                symbol_to_check = column[line]
+                if symbol != symbol_to_check:
+                    break
             else:
-                print("Amount must be greater than 0.")
-        else:
-            print("Please enter a number.")
-    return amount
+                winnings = values[symbol] * bet
+                winning_lines.append(lines + 1)
 
-def get_number_of_lines():
-    while True:
-        lines = input("Enter the number of lines to bet on(1-" + str(MAX_LINES) + ")? ")
-        if lines.isdigit():
-            lines = int(lines)
-            if 1 <= lines <= MAX_LINES:
-                break
+        return winnings, winning_lines
+
+    def get_slot_machine_spin(self, rows, cols, symbols):
+        all_symbols = []
+        for symbol, symbol_count in symbols.items():
+            for _ in range(symbol_count):
+                all_symbols.append(symbol)
+        columns = []
+        for col in range(cols):
+            column = []
+            current_symbols = all_symbols[:]
+            for row in range(rows):
+                value = random.choice(current_symbols)
+                current_symbols.remove(value)
+                column.append(value)
+            columns.append(column)
+
+        return columns
+
+    def print_slot_machine(self, columns):
+        for row in range(len(columns[0])):
+            for i, column in enumerate(columns):
+                if i != len(columns) - 1:
+                    print(column[row], end=" | ")
+                else:
+                    print(column[row], end="")
+            print()
+
+
+    def get_number_of_lines(self):
+        while True:
+            lines = input("Enter the number of lines to bet on(1-" + str(MAX_LINES) + ")? ")
+            if lines.isdigit():
+                lines = int(lines)
+                if 1 <= lines <= MAX_LINES:
+                    break
+                else:
+                    print("Enter a valid number of lines.")
             else:
-                print("Enter a valid number of lines.")
-        else:
-            print("Please enter a number.")
-    return lines
+                print("Please enter a number.")
+        return lines
 
-def get_bet():
-    while True:
-        amount = input("What would you like to bet on each line? $")
-        if amount.isdigit():
-            amount = int(amount)
-            if MIN_BET <= amount <= MAX_BET:
-                break
+    def get_bet(self):
+        while True:
+            amount = input("What would you like to bet on each line? $")
+            if amount.isdigit():
+                amount = int(amount)
+                if MIN_BET <= amount <= MAX_BET:
+                    break
+                else:
+                    print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
             else:
-                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
-        else:
-            print("Please enter a number.")
-    return amount
+                print("Please enter a number.")
+        return amount
 
-def spin(balance):
-    lines = get_number_of_lines()
-    while True:
-        bet = get_bet()
-        total_bet = bet * lines
-        if total_bet > balance:
-            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
-        else:
-            break
-    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
-    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
-    print_slot_machine(slots)
-    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
-    print(f"You won ${winnings}.")
-    print(f"You won on lines:", *winning_lines)
-    return winnings - total_bet
+    def spin(self, profile):
+        deposit_answer = input("Do you want to make a deposit? (y/n): ").lower()
+        if deposit_answer == "y":
+            self.deposit(profile)
+        lines = self.get_number_of_lines()
+        while True:
+            bet = self.get_bet()
+            total_bet = bet * lines
+            if total_bet > profile.balance:
+                print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+            else:
+                break
+        print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}")
+        slots = self.get_slot_machine_spin(ROWS, COLS, symbol_count)
+        self.print_slot_machine(slots)
+        winnings, winning_lines = self.check_winnings(slots, lines, bet, symbol_value)
+        print(f"You won ${winnings}.")
+        print(f"You won on lines:", *winning_lines)
+        profile.balance += winnings - total_bet
 
-def main():
-    balance = deposit()
-    while True:
-        print(f"Current balance is ${balance}")
-        answer = input("Press enter to play (q to quit).")
-        if answer == "q":
-            break
-        balance += spin(balance)
-    print(f"You are left with ${balance}")
+    def main(self):
+        slot_machine = SlotMachine()
+        while True:
+            profile_name = input("Enter your profile name (q to quit): ")
+            if profile_name.lower() == "q":
+                break
+            initial_balance = int(input("Enter the initial balance for the profile: "))
+            player_profile = slot_machine.create_profile(profile_name, initial_balance)
+            deposit_answer = input("Do you want to make a deposit? (y/n): ").lower()
+            if deposit_answer == "y":
+                player_profile.deposit()
+            while True:
+                print(player_profile)
+                answer = input("Press enter to play (q to quit).")
+                if answer.lower() == "q":
+                    print(f"{player_profile.name}, thank you for playing. Time for the next player.")
+                    break
+                self.spin(player_profile)
 
-main()
+        self.display_profiles()
+if __name__ == "__main__":
+    slot_machine = SlotMachine()
+    slot_machine.main()
